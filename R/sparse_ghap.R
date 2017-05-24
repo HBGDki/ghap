ea504d1a78e8945d9ee3059f94146c34da2f7b97
Jonathan Sidi <jonathan.sidi@PROD.GHAP.IO>
2017-05-24 22:09:19 +0000

    update sparse_ghap.R

--- R/sparse_ghap.R
+++ R/sparse_ghap.R
@@ -3,26 +3,28 @@
 #' @param repo_url character, repository url path
 #' @param repo character, path on local disk that repository is cloned to
 #' @param dirs character, vector of repository subdirectories to fetch
-#' @param new boolean, is this is new clone, Default: TRUE
+#' @param create boolean, create a new git clone?, Default: TRUE
 #' @param remote character, alias of the remote Default: 'origin'
 #' @param branch character, alias of the branch, Default: 'master'
 #' @return nothing
 #' @export
-sparse_ghap<-function(repo_url,repo,dirs,new=TRUE,remote='origin',branch='master'){
-  if(new){
+sparse_ghap<-function(repo_url,repo,dirs,create=TRUE,remote='origin',branch='master'){
+  if(create){
     # New repository
-    if(!dir.exists(repo)) dir.create(repo)
-    setwd(repo)
+    ghap_base=normalizePath(ghap::get_git_base_path(),winslash = '/')
+    repo_dir=file.path(ghap_base,repo)
+    if(!dir.exists(repo_dir)) dir.create(repo_dir)
+    setwd(repo_dir)
     system('git init')
-    system(sprintf('git remote add -f %s %s'),remote,repo_url)
+    system(sprintf('git remote add -f %s %s',remote,repo_url))
     system('git config core.sparsecheckout true')
-    system(sprintf('echo "%s" >> .git/info/sparse-checkout'),dirs)
+    cat(dirs,file = '.git/info/sparse-checkout',sep = '\n')
+    system(sprintf('git pull %s %s',remote,branch))
     system(sprintf('git branch --set-upstream-to=origin/%s master',branch))
-    system(sprintf('git pull %s %s'),remote,branch)
   }else{
     # Existing repository
     system('git config core.sparsecheckout true')
-    system(sprintf('echo "%s" >> .git/info/sparse-checkout'),dirs)
+    cat(dirs,file = '.git/info/sparse-checkout',sep = '\n',append = TRUE)
     system('git read-tree -mu HEAD')
     
     # If you later decide to change which directories you would like checked out, 
